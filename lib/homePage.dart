f03a2442ea7b0f49630118f42377ced1c6371788
GiaSumagang <99710450+GiaSumagang@users.noreply.github.com>
2022-12-21 17:50:26 +0800

    Changing the dismissible and added snackbar

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
-
 
 class Myhomepage extends StatefulWidget {
   const Myhomepage({Key? key, required String home}) : super(key: key);
@@ -23,7 +21,7 @@ class _MyhomepageState extends State<Myhomepage> {
   //READ
   fetchData() async {
     var response =
-    await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
 
     setState(() {
       todos = convert.jsonDecode(response.body);
@@ -34,35 +32,68 @@ class _MyhomepageState extends State<Myhomepage> {
   deleteData(var todo) async {
     var response = await http.delete(
         Uri.parse('https://jsonplaceholder.typicode.com/todos/${todo['id']}'));
+
+    if (response.statusCode == 200) {
+      showSuccessMessage('Deleted Successfully');
+      setState(() {
+        todos.remove(todo);
+      });
+    } else {
+      showErrorMessage('Request failed with a status: ${response.statusCode}');
+      throw Exception('Failed to delete todo');
+    }
+  }
+
+  displayEditedData(var object) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: Colors.deepPurple,
+        content: Text(
+            'Successfully edited Title: ${object["title"]} ID: ${object["id"]}')));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            leading: const Icon(Icons.list_alt_rounded),
-            title: const Text('Todo List')),
-        body: ListView.builder(
-            itemCount: todos.length,
-            itemBuilder: (context, index) {
-              final todo = todos[index];
+      appBar: AppBar(
+          leading: const Icon(Icons.list_alt_rounded),
+          title: const Text('Todo List')),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            final todo = todos[index];
 
-              return Dismissible(
-                direction: DismissDirection.endToStart,
-                background: Container(
-                  color: Colors.red,
-                ),
-                key: UniqueKey(),
-                child: ListTile(
-                    title: Text(todos.toString()),
-                ),
-                  onDismissed: (direction){
-                  setState(() {
-                    deleteData(todo['id']);
-                  });
-                  },
-              );
-            }),
+            return Dismissible(
+              direction: DismissDirection.endToStart,
+              background: Container(
+                color: Colors.red,
+              ),
+              key: UniqueKey(),
+              child: ListTile(
+                title: Text(todo['title']),
+              ),
+              onDismissed: (direction) {
+                setState(() {
+                  deleteData(todo);
+                });
+              },
+            );
+          }),
     );
   }
 
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
